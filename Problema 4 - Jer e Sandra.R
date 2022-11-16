b58f41f263082a2532f8f488c42a173adb93eb9a
SandraCarapeto <sandracarapetomv@gmail.com>
2022-11-16 16:48:30 +0000

    1ºPasso

--- Problema 4 - Jer e Sandra.R
+++ Problema 4 - Jer e Sandra.R
@@ -1,5 +1,12 @@
 library(tidyverse)
 library(data.table)
 
+#Nº de abates por mês/ano e kg por mês/ano por matadouro e exploração. 
+#Ver a evolução ao longo do tempo;
+
 Abates <- fread("./Abates.csv") 
 Abates
+Abates <- select(Abates,Data_abate,Matadouro,Exploracao,Peso)
+Abates_numero <- select(Abates,Data_abate,Matadouro,Exploracao)
+Abates_numero_por_data <- arrange(Abates_numero,Data_abate)
+#Próximo passo dividir a data em três colunas
