edf2a8098c1c2835e2a41e5709e2a18c11b0ca72
Inês Cascais <ines.s.cascais@gmail.com>
2022-11-17 11:32:23 +0000

    Update Problema 4 - Cris e Inês.R
    grafico freq tipo de abate

--- Problema 4 - Cris e Inês.R
+++ Problema 4 - Cris e Inês.R
@@ -1,7 +1,14 @@
 library(tidyverse)
 library(data.table)
+library(plotly)
 
 Abates <- fread("./Abates.csv") 
 Abates
 
 Abates1 <- select(Abates, Matadouro, MAC, Tipo_abate, Exploracao, Raca)
+
+graph_tipo <- plot_ly(data = count(Abates1, Tipo_abate), 
+                      labels = ~Tipo_abate, values = ~n, type = "pie") %>% 
+              layout(title = "Tipo de Abates")
+
+graph_tipo
