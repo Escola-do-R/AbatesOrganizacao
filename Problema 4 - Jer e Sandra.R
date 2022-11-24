aed3ccf66835a1ce26e1d915d475e177456013a9
JerFreud <jerfreudenthal@gmail.com>
2022-11-24 13:48:56 +0000

    Adicionei o que tinha feito
    Basicamente fiz abates por ano/mês, matadouro e exploração. Já tento representar graficamente.

--- Problema 4 - Jer e Sandra.R
+++ Problema 4 - Jer e Sandra.R
@@ -1,12 +1,29 @@
 library(tidyverse)
 library(data.table)
+library(data.table)
+library(dplyr)
+library(ggplot2)
 
 #Nº de abates por mês/ano e kg por mês/ano por matadouro e exploração. 
 #Ver a evolução ao longo do tempo;
 
 Abates <- fread("./Abates.csv") 
-Abates
+
 Abates <- select(Abates,Data_abate,Matadouro,Exploracao,Peso)
 Abates_numero <- select(Abates,Data_abate,Matadouro,Exploracao)
 Abates_numero_por_data <- arrange(Abates_numero,Data_abate)
 #Próximo passo dividir a data em três colunas
+
+Abates_by_Expl <- Abates %>% 
+  group_by(Exploracao) %>% 
+  summarise(numero_abates = n())
+
+Abates_by_Time <- Abates %>% 
+  mutate(Data_abate = as.Date(Data_abate),
+         Year_Month = format(Data_abate, "%Y-%m")) %>% 
+  group_by(Year_Month) %>% 
+  summarise(numero_abates_data = n())
+
+Abates_by_Matadouro <- Abates %>% 
+  group_by(Matadouro) %>% 
+  summarise(numero_abates = n())
